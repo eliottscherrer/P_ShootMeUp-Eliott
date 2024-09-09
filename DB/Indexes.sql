9d73e4502a3778301f9a4d2d1bfdc4d475ce4cec
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-09-09 14:46:03 +0200

    Create Indexes.sql

---
+++ DB/Indexes.sql
@@ -0,0 +1,10 @@
+-- En étudiant le dump MySQL db_space_invaders.sql vous constaterez que vous ne trouvez pas le mot clé INDEX. 
+-- 1. Pourtant certains index existent déjà. Pourquoi ? 
+
+
+-- 2. Quels sont les avantages et les inconvénients des index ?
+
+
+-- 3. Sur quel champ (de quelle table), cela pourrait être pertinent d’ajouter un index ? 
+-- Justifier votre réponse.
+
