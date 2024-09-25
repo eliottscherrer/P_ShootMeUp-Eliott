b1bf386819d05eb441ed83d956dda8f1f5039a07
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-09-25 13:49:18 +0200

    typo(POO): Moved SLN to src directory

--- POO/src/ShootMeUpV2/GameRoot.cs
+++ POO/src/ShootMeUpV2/GameRoot.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace ShootMeUpV2
 {
-    public class Game1 : Game
+    public class GameRoot : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        public GameRoot()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
