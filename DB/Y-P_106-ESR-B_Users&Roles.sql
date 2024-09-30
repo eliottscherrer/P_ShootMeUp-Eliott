0a454ba67f176adee9f70e62cb8a9363829cf2a1
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-09-30 14:29:42 +0200

    fix(DB): Removed @'localhost' in users/roles creation

--- DB/Y-P_106-ESR-B_Users&Roles.sql
+++ DB/Y-P_106-ESR-B_Users&Roles.sql
@@ -7,45 +7,45 @@ USE db_space_invaders;
 CREATE ROLE 'Administrateurs';
 -- Peut créer, lire, mettre à jour et supprimer (CRUD) n'importe quelle table.
 -- Gérer les utilisateurs et leurs privilèges
-GRANT CREATE, SELECT, UPDATE, DELETE ON * TO 'Administrateurs'@'localhost' WITH GRANT OPTION;
+GRANT CREATE, SELECT, UPDATE, DELETE ON * TO 'Administrateurs' WITH GRANT OPTION;
 
 -- Joueurs --
 
 CREATE ROLE 'Joueurs';
 -- Lire les informations des armes (pour voir quelles armes il peut acheter).
-GRANT SELECT ON t_arme TO 'Joueurs'@'localhost';
+GRANT SELECT ON t_arme TO 'Joueurs';
 -- Créer une commande.
 -- Lire toutes les commandes.
-GRANT CREATE, SELECT ON t_commande TO 'Joueurs'@'localhost';
+GRANT CREATE, SELECT ON t_commande TO 'Joueurs';
 
 -- Gestionnaires de la boutique --
 
 CREATE ROLE 'GestionnairesBoutique';
 -- Lire les informations sur tous les joueurs (pour savoir qui a passé une commande).
-GRANT SELECT ON t_joueur TO 'GestionnairesBoutique'@'localhost';
+GRANT SELECT ON t_joueur TO 'GestionnairesBoutique';
 -- Mettre à jour, lire et supprimer des armes (ajout de nouvelles armes, modification des prix, etc.).
-GRANT UPDATE, SELECT, DELETE ON t_arme TO 'GestionnairesBoutique'@'localhost';
+GRANT UPDATE, SELECT, DELETE ON t_arme TO 'GestionnairesBoutique';
 -- Lire toutes les commandes.
-GRANT SELECT ON t_commande TO 'GestionnairesBoutique'@'localhost';
+GRANT SELECT ON t_commande TO 'GestionnairesBoutique';
 
 -----------------------------------------------------------------------------------------------
 ------------------------------------------ [ USERS ] ------------------------------------------
 -----------------------------------------------------------------------------------------------
 
 -- Administrateur --
-CREATE USER 'Administrateur'@'localhost' IDENTIFIED BY 'adminpass';
-GRANT 'Administrateurs'@'localhost' TO 'Administrateur'@'localhost';
-SET DEFAULT ROLE 'Administrateurs' FOR 'Administrateur'@'localhost';
+CREATE USER 'Administrateur' IDENTIFIED BY 'adminpass';
+GRANT 'Administrateurs' TO 'Administrateur';
+SET DEFAULT ROLE 'Administrateurs' FOR 'Administrateur';
 
 -- Joueur --
-CREATE USER 'Joueur1'@'localhost' IDENTIFIED BY 'joueur1pass';
-GRANT 'Joueurs'@'localhost' TO 'Joueur1'@'localhost';
-SET DEFAULT ROLE 'Joueurs' FOR 'Joueur1'@'localhost';
+CREATE USER 'Joueur1' IDENTIFIED BY 'joueur1pass';
+GRANT 'Joueurs' TO 'Joueur1';
+SET DEFAULT ROLE 'Joueurs' FOR 'Joueur1';
 
 -- Gestionnaire de la boutique --
-CREATE USER 'GestionnaireBoutique'@'localhost' IDENTIFIED BY 'gestionnairepass';
-GRANT 'GestionnairesBoutique'@'localhost' TO 'GestionnaireBoutique'@'localhost';
-SET DEFAULT ROLE 'GestionnairesBoutique' FOR 'GestionnaireBoutique'@'localhost';
+CREATE USER 'GestionnaireBoutique' IDENTIFIED BY 'gestionnairepass';
+GRANT 'GestionnairesBoutique' TO 'GestionnaireBoutique';
+SET DEFAULT ROLE 'GestionnairesBoutique' FOR 'GestionnaireBoutique';
 
 FLUSH PRIVILEGES;
 
@@ -54,3 +54,6 @@ FLUSH PRIVILEGES;
 -----------------------------------------------------------------------------------------------
 
 -- Montrer les privilèges de chaque utilisateurs
+SHOW GRANTS FOR 'Administrateur';
+SHOW GRANTS FOR 'Joueur1';
+SHOW GRANTS FOR 'GestionnaireBoutique';
