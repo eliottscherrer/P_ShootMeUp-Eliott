f1e7969ed816ca06fa48297fe1d979f5d17e2f25
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-10-09 14:32:37 +0200

    refactor(POO): Add EnemyMovementLogic class

---
+++ POO/src/ShootMeUpV1/Entity/Components/EnemyMovementLogic.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ShootMeUpV1
+{
+    public class EnemyMovementLogic : IMovementLogic
+    {
+        public Vector2 GetMovementDirection(Entity entity)
+        {
+            // If colliding with the player, stop moving
+            if (entity.GetComponent<CollisionComponent>().IsCollidingWith(EntityManager.LocalPlayer))
+                return Vector2.Zero;
+
+            Vector2 playerPosition = EntityManager.LocalPlayer.Position;
+            Vector2 directionToPlayer = playerPosition - entity.Position;
+
+            directionToPlayer.Normalize();
+
+            return directionToPlayer;
+        }
+    }
+}
