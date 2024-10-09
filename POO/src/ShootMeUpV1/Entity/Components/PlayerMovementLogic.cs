f525b117383a5c73a9aa4284c9b6e81b033dc502
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-10-09 14:33:18 +0200

    refactor(POO): Add PlayerMovementLogic class

---
+++ POO/src/ShootMeUpV1/Entity/Components/PlayerMovementLogic.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace ShootMeUpV1
+{
+    public class PlayerMovementLogic : IMovementLogic
+    {
+        public Vector2 GetMovementDirection(Entity entity)
+        {
+            Vector2 direction = Vector2.Zero;
+            if (InputManager.IsKeyDown(Keys.Left) || InputManager.IsKeyDown(Keys.A))
+                direction.X -= 1;
+            if (InputManager.IsKeyDown(Keys.Right) || InputManager.IsKeyDown(Keys.D))
+                direction.X += 1;
+            if (InputManager.IsKeyDown(Keys.Up) || InputManager.IsKeyDown(Keys.W))
+                direction.Y -= 1;
+            if (InputManager.IsKeyDown(Keys.Down) || InputManager.IsKeyDown(Keys.S))
+                direction.Y += 1;
+
+            return direction.Length() > 0 ? Vector2.Normalize(direction) : Vector2.Zero;
+        }
+    }
+}
