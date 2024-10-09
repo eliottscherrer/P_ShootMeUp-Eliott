b034045e2680759b22de3de55f65183aceb1f6b7
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-10-09 13:45:44 +0200

    feat(POO): Add RenderComponent

---
+++ POO/src/ShootMeUpV1/Entity/Components/RenderComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ShootMeUpV1
+{
+    public class RenderComponent : IDrawableComponent
+    {
+        private Entity _entity;
+        private Texture2D _texture;
+        private float _scale;
+        private Color _color;
+        public Vector2 Size => (_texture?.Bounds.Size.ToVector2() ?? Vector2.Zero) * _scale;
+
+        public RenderComponent(Texture2D texture, float scale = 1f, Color? color = null)
+        {
+            _texture = texture;
+            _scale = scale;
+            _color = color ?? Color.White;
+        }
+
+        public void Initialize(Entity entity)
+        {
+            _entity = entity;
+        }
+
+        public void Draw()
+        {
+            if (_texture != null)
+            {
+                GameRoot.SpriteBatch.Draw(_texture, _entity.Position, null, _color, _entity.Rotation, Vector2.Zero, _scale, SpriteEffects.None, 0f);
+            }
+        }
+    }
+}
