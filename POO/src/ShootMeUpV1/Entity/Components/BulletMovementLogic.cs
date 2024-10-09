136b9d347574fda697aff1ebaaa5f1a036b2d8dd
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-10-09 14:28:39 +0200

    refactor(POO): Add BulletMovementLogic class

---
+++ POO/src/ShootMeUpV1/Entity/Components/BulletMovementLogic.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ShootMeUpV1
+{
+    public class BulletMovementLogic : IMovementLogic
+    {
+        // Bullets maintain their initial velocity; they do not change direction.
+        public Vector2 GetMovementDirection(Entity entity) => entity.Velocity;
+    }
+
+}
