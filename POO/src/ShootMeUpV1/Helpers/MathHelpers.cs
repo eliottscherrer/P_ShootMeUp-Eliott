e5cdec8503542183a611204c6b7cc2c70c0e4c6c
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-10-09 15:20:23 +0200

    refactor(POO): Rename VectorHelpers to MathHelpers

--- POO/src/ShootMeUpV1/Helpers/MathHelpers.cs
+++ POO/src/ShootMeUpV1/Helpers/MathHelpers.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace ShootMeUpV1
 {
-    public static class VectorHelpers
+    public static class MathHelpers
     {
         // Vector to angle in radians
         public static float ToAngle(this Vector2 vector) => (float)Math.Atan2(vector.Y, vector.X);
