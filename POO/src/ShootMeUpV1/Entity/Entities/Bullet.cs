99b14b0e6f439cfec300c9bfe60b881c41b0936c
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-10-09 15:01:01 +0200

    refactor(POO): Implement CBS for Bullet class

--- POO/src/ShootMeUpV1/Entity/Entities/Bullet.cs
+++ POO/src/ShootMeUpV1/Entity/Entities/Bullet.cs
@@ -5,38 +5,25 @@ using System;
 
 namespace ShootMeUpV1
 {
-    public enum BulletType
-    {
-        Player,
-        Enemy,
-        SIZE
-    }
 
     public class Bullet : Entity
     {
-        ///////////////////////////////// [ CONSTS ] /////////////////////////////////
-
-        public const float DEFAULT_SPEED = 5f;
-        public const int DEFAULT_DAMAGE = 10;
-        private const float SCALE = 0.065f;
-        private const float COLLISION_RADIUS = 3f;
-
-        ////////////////////////////////// [ VARS ] //////////////////////////////////
-
-        public readonly BulletType Type;
-        public readonly int Damage;
-
-        //////////////////////////////////////////////////////////////////////////////
-
-        public Bullet(Vector2 position, Vector2 velocity, BulletType type, int damage = DEFAULT_DAMAGE) : base(position, velocity)
+        public enum Type
         {
-            Scale = SCALE;
-            CollisionRadius = COLLISION_RADIUS;
-            Texture = Visuals.SwordSlash;
-            Rotation = Velocity.ToAngle();
-            Rotation += MathHelper.PiOver2; // Add 90 degrees because the texture is not properly oriented
-            Type = type;
-            Damage = damage;
+            LocalPlayer,
+            Enemy,
+            SIZE
+        }
+
+        public Bullet(Vector2 position, Vector2 velocity) : base(position)
+        {
+            Velocity = velocity;
+            Rotation = velocity.ToAngle() + MathHelper.PiOver2;             // Adjust with Pi/2 because the texture is pointed up
+
+            AddComponent(new RenderComponent(Visuals.SwordSlash, 0.065f));  // Texture
+            AddComponent(new CollisionComponent(6f));                       // Collision radius
+
+            // TODO: Debug infos
         }
 
         public override void Update(GameTime gameTime)
@@ -44,33 +31,12 @@ namespace ShootMeUpV1
             // Move
             Position += Velocity;
 
-            // Delete when out of bounds
-            if (this.IsOutOfBounds()) // "this" is optional but I wrote it for clarity
-                IsDestroyed = true;
+            // TODO: Destroy if it goes out of bounds
         }
 
-        // Events
         public override void OnCollision(Entity other)
         {
-            // Handle the collision based on the type of the other entity
-            // Only considers bullets that aren't from allies
-            switch (other)
-            {
-                case LocalPlayer player when this.Type == BulletType.Enemy:
-                    this.IsDestroyed = true;
-                    player.TakeDamage(Damage);
-                    break;
-
-                case Enemy enemy when this.Type == BulletType.Player:
-                    this.IsDestroyed = true;
-                    enemy.TakeDamage(Damage);
-                    break;
-
-                case Bullet bullet:
-                    this.IsDestroyed = true;
-                    bullet.IsDestroyed = true;
-                    break;
-            }
+            throw new NotImplementedException();
         }
     }
 }
