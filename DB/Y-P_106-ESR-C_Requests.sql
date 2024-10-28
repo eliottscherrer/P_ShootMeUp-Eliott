deff137028d150c5024cc26d05e6f07433ce7d04
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-10-28 12:32:28 +0100

    feat(DB): Added comments to explain the requests

--- DB/Y-P_106-ESR-C_Requests.sql
+++ DB/Y-P_106-ESR-C_Requests.sql
@@ -1,64 +1,74 @@
--- Requete 1
+-- ------------------------------------------------------------------------
+-- --------------------------- [ Requete 1 ] ------------------------------
+-- ------------------------------------------------------------------------
 -- Sélectionner les 5 joueurs qui ont le meilleur score c’est-à-dire qui ont le nombre de points
 -- le plus élevé. Les joueurs doivent être classés dans l’ordre décroissant
 SELECT
-    *
+    * -- Sélectionne toutes les colonnes de la table.
 FROM
-    t_joueur
+    t_joueur -- Spécifie la table source "t_joueur" pour récupérer les données des joueurs.
 ORDER BY
-    jouNombrePoints
-    DESC
+    jouNombrePoints -- Trie les joueurs par "jouNombrePoints" (nombre de points) en ordre décroissant.
+    DESC -- Indique un tri décroissant pour afficher les plus hauts scores en premier.
 LIMIT
-    5;
+    5; -- Limite la sélection aux 5 premiers résultats.
 
--- Requete 2
+-- ------------------------------------------------------------------------
+-- --------------------------- [ Requete 2 ] ------------------------------
+-- ------------------------------------------------------------------------
 -- Trouver le prix maximum, minimum et moyen des armes.
 -- Les colonnes doivent avoir pour nom « PrixMaximum », « PrixMinimum » et « PrixMoyen)
 SELECT
-    MAX(armPrix) AS 'PrixMaximum',
-    MIN(armPrix) AS 'PrixMinimum',
-    AVG(armPrix) AS 'PrixMoyen'
+    MAX(armPrix) AS PrixMaximum, -- Récupère le prix maximum de "armPrix" avec l'alias "PrixMaximum".
+    MIN(armPrix) AS PrixMinimum, -- Récupère le prix minimum de "armPrix" avec l'alias "PrixMinimum".
+    AVG(armPrix) AS PrixMoyen -- Calcule le prix moyen de "armPrix" avec l'alias "PrixMoyen".
 FROM
     t_arme;
 
--- Requete 3
+-- ------------------------------------------------------------------------
+-- --------------------------- [ Requete 3 ] ------------------------------
+-- ------------------------------------------------------------------------
 -- Trouver le nombre total de commandes par joueur et trier du plus grand nombre au plus petit.
 -- La 1ère colonne aura pour nom "IdJoueur", la 2ème colonne aura pour nom "NombreCommandes"
 SELECT 
-    fkJoueur AS 'IdJoueur',
-    COUNT(fkJoueur) AS 'NombreCommandes'
+    fkJoueur AS IdJoueur, -- "fkJoueur" avec alias pour identifier chaque joueur.
+    COUNT(fkJoueur) AS NombreCommandes -- Compte le nombre total de commandes par joueur.
 FROM 
     t_commande
 GROUP BY 
-    fkJoueur
+    fkJoueur -- Groupe par joueur.
 ORDER BY 
-    NombreCommandes DESC;
+    NombreCommandes DESC; -- Trie pour les joueurs ayant le plus de commandes.
 
--- Requete 4
+-- ------------------------------------------------------------------------
+-- --------------------------- [ Requete 4 ] ------------------------------
+-- ------------------------------------------------------------------------
 -- Trouver les joueurs qui ont passé plus de 2 commandes.
 -- La 1ère colonne aura pour nom "IdJoueur", la 2ème colonne aura pour nom "NombreCommandes"
 SELECT 
-    fkJoueur AS 'IdJoueur',
-    COUNT(fkJoueur) AS 'NombreCommandes'
+    fkJoueur AS IdJoueur,
+    COUNT(fkJoueur) AS NombreCommandes -- Compte les commandes par joueur.
 FROM 
     t_commande
 GROUP BY 
     fkJoueur
 HAVING
-    NombreCommandes > 2
+    NombreCommandes > 2 -- Inclut les joueurs avec plus de 2 commandes.
 ORDER BY 
     NombreCommandes DESC;
 
--- Requete 5
+-- ------------------------------------------------------------------------
+-- --------------------------- [ Requete 5 ] ------------------------------
+-- ------------------------------------------------------------------------
 -- Trouver le pseudo du joueur et le nom de l'arme pour chaque commande.
 SELECT 
-    j.jouPseudo,
-    a.armNom
+    j.jouPseudo, -- Affiche le pseudo du joueur.
+    a.armNom -- Affiche le nom de l'arme.
 FROM 
     t_commande c
-    INNER JOIN t_joueur j ON c.fkJoueur = j.idJoueur
-    INNER JOIN t_detail_commande dc ON c.idCommande = dc.fkCommande
-    INNER JOIN t_arme a ON dc.fkArme = a.idArme;
+    INNER JOIN t_joueur j ON c.fkJoueur = j.idJoueur -- Joint les joueurs aux commandes.
+    INNER JOIN t_detail_commande dc ON c.idCommande = dc.fkCommande -- Joint les détails de commande.
+    INNER JOIN t_arme a ON dc.fkArme = a.idArme; -- Joint les armes pour obtenir leur nom.
 
 -- ou
 
@@ -66,7 +76,7 @@ SELECT
     jouPseudo, arm.armNom
 FROM
     t_joueur jo,
-    t_commande co
+    t_commande co,
     t_detail_commande dc,
     t_arme arm
 WHERE
@@ -74,13 +84,15 @@ WHERE
     dc.fkCommande = co.idCommande AND
     dc.fkArme = arm.idArme;
 
--- Requete 6
+-- ------------------------------------------------------------------------
+-- --------------------------- [ Requete 6 ] ------------------------------
+-- ------------------------------------------------------------------------
 -- Trouver le total dépensé par chaque joueur en ordonnant par le montant le plus élevé en 
 -- premier, et limiter aux 10 premiers joueurs.
 -- La 1ère colonne doit avoir pour nom "IdJoueur" et la 2ème colonne "TotalDepense"
 SELECT 
-    c.fkJoueur AS 'IdJoueur',
-    SUM(a.armPrix * dc.detQuantiteCommande) AS 'TotalDepense'
+    c.fkJoueur AS IdJoueur,
+    SUM(a.armPrix * dc.detQuantiteCommande) AS TotalDepense -- Calcule le total dépensé par joueur.
 FROM 
     t_commande c
     INNER JOIN t_detail_commande dc ON c.idCommande = dc.fkCommande
@@ -91,13 +103,15 @@ ORDER BY
     TotalDepense DESC
 LIMIT 10;
 
--- Requete 7
+-- ------------------------------------------------------------------------
+-- --------------------------- [ Requete 7 ] ------------------------------
+-- ------------------------------------------------------------------------
 -- Récupérez tous les joueurs et leurs commandes, même s'ils n'ont pas passé de commande.
 -- Dans cet exemple, même si un joueur n'a jamais passé de commande, il sera quand 
 -- même listé, avec des valeurs `NULL` pour les champs de la table `t_commande`.
 SELECT 
-    j.*,
-    c.*
+    j.*, -- Sélectionne toutes les colonnes de la table "t_joueur" raccourcie t
+    c.* -- Sélectionne toutes les colonnes de la table "t_commande" raccourcie c
 FROM 
     t_joueur j
     LEFT JOIN t_commande c ON j.idJoueur = c.fkJoueur
@@ -107,13 +121,15 @@ FROM
 -- ou 
 
 SELECT
-    jou.*, co.*
+    j.*, co.*
 FROM
-    t_joueur jou
-    LEFT JOIN t_commande co ON jou.idJoueur = co.fkJoueur
+    t_joueur j
+    LEFT JOIN t_commande co ON j.idJoueur = co.fkJoueur
     LEFT JOIN t_detail_commande dc ON co.idCommande = dc.fkCommande;
 
--- Requete 8
+-- ------------------------------------------------------------------------
+-- --------------------------- [ Requete 8 ] ------------------------------
+-- ------------------------------------------------------------------------
 -- Récupérer toutes les commandes et afficher le pseudo du joueur s’il existe, sinon afficher
 -- `NULL` pour le pseudo. 
 SELECT 
@@ -123,12 +139,14 @@ FROM
     t_commande c
     LEFT JOIN t_joueur j ON c.fkJoueur = j.idJoueur;
 
--- Requete 9
+-- ------------------------------------------------------------------------
+-- --------------------------- [ Requete 9 ] ------------------------------
+-- ------------------------------------------------------------------------
 -- Trouver le nombre total d'armes achetées par chaque joueur (même si ce joueur n'a acheté aucune Arme)
 SELECT 
     j.idJoueur, 
     j.jouPseudo, 
-    IFNULL(SUM(dc.detQuantiteCommande), 0) AS totalArmesAchetees
+    IFNULL(SUM(dc.detQuantiteCommande), 0) AS totalArmesAchetees -- Somme des quantités d'armes achetées, en affichant 0 pour les joueurs sans achats.
 FROM 
     t_joueur j
 LEFT JOIN 
@@ -138,11 +156,13 @@ LEFT JOIN
 GROUP BY 
     j.idJoueur, j.jouPseudo;
 
--- Requete 10
+-- ------------------------------------------------------------------------
+-- --------------------------- [ Requete 10 ] ------------------------------
+-- ------------------------------------------------------------------------
 -- Trouver les joueurs qui ont acheté plus de 3 types d'armes différentes.
 SELECT 
     j.*,
-    COUNT(DISTINCT dc.fkArme) AS NombreArmesDifferentesAchetees
+    COUNT(DISTINCT dc.fkArme) AS NombreArmesDifferentesAchetees -- Compte le nombre distinct d'armes achetées.
 FROM 
     t_joueur j
 INNER JOIN 
@@ -152,4 +172,4 @@ INNER JOIN
 GROUP BY 
     j.idJoueur, j.jouPseudo
 HAVING 
-    NombreArmesDifferentesAchetees > 3;
+    COUNT(DISTINCT dc.fkArme) > 3;
