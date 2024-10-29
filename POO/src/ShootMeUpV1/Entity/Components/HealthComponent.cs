d00e1bec267d21855ebf83f28a7aaf450e0f7026
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-10-29 14:08:13 +0100

    feat(POO): Create health component

---
+++ POO/src/ShootMeUpV1/Entity/Components/HealthComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace ShootMeUpV1
+{
+    public class HealthComponent : IUpdatableComponent
+    {
+        private Entity _entity;
+        private float _maxHealth;
+        public float CurrentHealth { get; private set; }
+        // TODO: Add invulnerability
+        //       Add regeneration
+        //       Add death events
+
+        public HealthComponent(float maxHealth)
+        {
+            _maxHealth = maxHealth;
+            CurrentHealth = maxHealth;
+        }
+
+        public void Initialize(Entity entity)
+        {
+            _entity = entity;
+        }
+
+        public void TakeDamage(float amount)
+        {
+        }
+
+        public void Heal(float amount)
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+        }
+    }
+}
