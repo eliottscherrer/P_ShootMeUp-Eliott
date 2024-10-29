20a3ddfee13a64f85a20fa459b1442f7683c43e9
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-10-29 14:35:53 +0100

    refactor(POO): Add GetDirectionTo extension method for vectors

--- POO/src/ShootMeUpV1/Entity/Entities/LocalPlayer.cs
+++ POO/src/ShootMeUpV1/Entity/Entities/LocalPlayer.cs
@@ -6,7 +6,6 @@ namespace ShootMeUpV1
     {
         public LocalPlayer(Vector2 position) : base(position)
         {
-            // TODO: Config files
             AddComponent(new MovementComponent(new PlayerMovementLogic(), 300f));   // Speed
             AddComponent(new RenderComponent(Visuals.Player, 0.25f));               // Player texture
             AddComponent(new CollisionComponent(50f));                              // Collision radius
@@ -27,17 +26,11 @@ namespace ShootMeUpV1
         private void FireBullet()
         {
             // TODO: Change the start position so it's not on top left
-            Vector2 bulletStartPosition = Position;
+            Vector2 startPosition = Position;
 
             // Create and add the bullet entity
-            EntityManager.Add(new Bullet(bulletStartPosition, GetAimDirection()));
-        }
-
-        private Vector2 GetAimDirection()
-        {
-            // Get the direction from the player to the mouse position
-            Vector2 direction = InputManager.MousePosition - Position;
-            return direction != Vector2.Zero ? Vector2.Normalize(direction) : Vector2.Zero;
+            Vector2 direction = Position.GetDirectionTo(InputManager.MousePosition);
+            EntityManager.Add(new Bullet(startPosition, direction));
         }
 
         public override void OnCollision(Entity other)
--- POO/src/ShootMeUpV1/Helpers/MathHelpers.cs
+++ POO/src/ShootMeUpV1/Helpers/MathHelpers.cs
@@ -9,5 +9,7 @@ namespace ShootMeUpV1
         public static float ToAngle(this Vector2 vector) => (float)Math.Atan2(vector.Y, vector.X);
 
         public static bool IsInRange(this float value, float min, float max) => value >= min && value <= max;
+
+        public static Vector2 GetDirectionTo(this Vector2 from, Vector2 to) => to - from != Vector2.Zero ? Vector2.Normalize(to - from) : Vector2.Zero;
     }
 }
