1deb7bc58b9ffe070e9d3d2fdcfc669da6bae35e
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-11-01 11:39:08 +0100

    feat(POO): Create ProtectionComponent

---
+++ POO/src/ShootMeUpV1/Entity/Components/ProtectionComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShootMeUpV1
+{
+    public class ProtectionComponent : IUpdatableComponent
+    {
+        private Entity _entity;
+
+        public void Initialize(Entity entity)
+        {
+            _entity = entity;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
