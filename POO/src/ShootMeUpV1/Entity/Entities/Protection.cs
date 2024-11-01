d8056ef8e8ebdcb603aefc3c978adb850417fa5e
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-11-01 11:56:35 +0100

    feat(POO): Create Protection entity

---
+++ POO/src/ShootMeUpV1/Entity/Entities/Protection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace ShootMeUpV1
+{
+    public class Protection : Entity
+    {
+        public Protection(Vector2 position, float rotation) : base(position)
+        {
+            Rotation = rotation;
+
+            AddComponent(new RenderComponent(Visuals.Protection, Configs.Protection.Scale));
+            AddComponent(new CollisionComponent(Configs.Protection.CollisionRadius));
+        }
+    }
+}
